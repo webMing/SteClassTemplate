cddecebb9930aca125f68cb49cefa68955ad1087
RCTechnology <13663788159@163.com>
2017-09-30 14:50:53 +0800

    first time commit all xcode templates

---
+++ SteViewControllerObjective-C/___FILEBASENAME___.m
@@ -0,0 +1,49 @@
+//___FILEHEADER___
+
+#import "___FILEBASENAME___.h"
+
+@interface ___FILEBASENAMEASIDENTIFIER___ ()
+
+@end
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+#pragma mark- LifeCicle
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self steConfigureView];
+    [self steAddSubViews];
+    [self steLayoutSubViews];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+#pragma mark- CreateUI
+
+- (void)steConfigureView {
+   
+}
+
+- (void)steAddSubViews {
+   
+}
+
+- (void)steLayoutSubViews {
+    
+}
+
+#pragma mark- EventRespone
+
+#pragma mark- CustomDelegateMethod
+
+#pragma mark- DelegateMethod
+
+#pragma mark- GetterAndSetter
+
+#pragma mark- PrivateMethod
+
+@end
